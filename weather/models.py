75f116d405d7b328154f4bf14730141c343fb1ff
jonathan colindres <jcolindres.cs@gmail.com>
2019-08-06 23:41:48 -0400

    need to fix weather problem

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+#alows to make city name table
+class City(models.Model):
+    name = models.CharField(max_length = 25)
+
+    def __str__(self):
+        return self.name
+    #city ies instead of s
+    class Meta:
+        verbose_name_plural = 'cities'
--- weather/views.py
+++ weather/views.py
@@ -1,24 +1,29 @@
 from django.shortcuts import render
 import requests
+from .models import City
 #af5b7e6f7bfea30b47ccac1ef034a00e api key
 # Create your views here.
 def index(request):
     #the url form the websit
-    url= 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
+    url= 'https://samples.openweathermap.org/data/2.5/weather?q={}&appid=b6907d289e10d714a6e88b30761fae22'
     #this what we will pass in
-    city= 'Las Vegas'
-    #we are calling the url to get the information from the city city
-    r = requests.get(url.format(city)).json()
+    city="london"
     #prints the info on the commandline
+    weather_data=[]
     #print(response.text)
-    #the infor that we want to extract from the api
-    city_weather = {
-        'city': city ,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'] ,
-        'icon': r['weather'][0]['icon'],
-    }
+    cities=City.objects.all()
+    for city in cities:
+        #the infor for the city
+        r= requests.get(url.format(city)).json()
+        #the specific infor that we want to extract from the api
+        city_weather = {
+            'city': city.name ,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'] ,
+            'icon': r['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
     #we want to place thls infor to the interface
-    context = {'city_weather':city_weather}
+    context = {'weather_data':weather_data}
     #to the template
     return render(request,'weather/weather.html',context)
