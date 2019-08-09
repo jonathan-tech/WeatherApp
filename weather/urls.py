4cc187f8e3e61fbf9e7014c8148e6bf484eed57a
jonathan colindres <jcolindres.cs@gmail.com>
2019-08-09 10:45:01 -0400

    the weather app is working

--- weather/urls.py
+++ weather/urls.py
@@ -5,4 +5,5 @@ from .import views
 
 urlpatterns = [
     path('',views.index,name="index"),
+    path('delete/<city_name>/', views.delete_city, name='delete_city')
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from .models import City
 from .forms import CityForm
@@ -8,10 +8,36 @@ def index(request):
     #the url form the website, use the api key and paste it to the end. units as imperial to get regular degrees
     url= 'https://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=af5b7e6f7bfea30b47ccac1ef034a00e'
 
+    err_msg=''
+    #hold message user will see
+    message = ''
+    #css color message area
+    message_class=''
 
     if request.method == "POST":
         form = CityForm(request.POST)
-        form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            #will get the city that exist
+            existing_city_count = City.objects.filter(name=new_city).count()
+            if existing_city_count == 0:
+                #the infor for the city json will convert it to dictionarys
+                r= requests.get(url.format(new_city)).json()
+                #api cod==200 means city has beedn found
+                if r['cod']==200:
+                    form.save()
+                else:
+                    err_msg='This city doesnt exist'
+            else:
+                err_msg="city already on the list"
+
+        if err_msg:
+            message= err_msg
+            message_class='is-danger'
+        else:
+            message='City is added successfuly'
+            message_class= 'is-success'
 
     form = CityForm()
     #prints the info on the commandline
@@ -31,6 +57,15 @@ def index(request):
         }
         weather_data.append(city_weather)
     #we want to place thls infor to the interface
-    context = {'weather_data':weather_data, 'form':form}
+    context = {
+        'weather_data':weather_data,
+        'form':form,
+        'message': message,
+        'message_class':message_class
+    }
     #to the template
     return render(request,'weather/weather.html',context)
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+    return redirect('index')
